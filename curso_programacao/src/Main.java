f0e45df6420fb1d684987e7052a164375ee51a93
Hudson Amorim <hudson.amorim@ML.MEC>
2022-03-18 10:03:15 -0300

    Exercicio em Java Calculo de circunfência

---
+++ curso_programacao/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner (System.in);
+	
+	double raio  = sc.nextDouble();
+	double area = 3.14159 * raio*2;
+	
+	
+	System.out.println("Area do circulo :" +area);
+	
+		
+	}
+
+}
